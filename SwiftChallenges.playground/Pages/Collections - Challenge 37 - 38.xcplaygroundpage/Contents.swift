27e1ab68d427cc93dd6f2f34eb40ed918fc5a79c
laurent <wrmultitudes@gmail.com>
2020-01-23 14:56:42 +0100

    Challenge 41: Find the median

--- SwiftChallenges.playground/Pages/Collections - Challenge 37 - 38.xcplaygroundpage/Contents.swift
+++ SwiftChallenges.playground/Pages/Collections - Challenge 37 - 38.xcplaygroundpage/Contents.swift
@@ -186,3 +186,60 @@ extension Collection where Iterator.Element == String {
  
  [Next](@next)
 */
+
+func challenge40(array: [Int]) -> [Int] {
+    var temp = Set(1...100)
+    //formSymmetricDifference returns the elements which are in temp but missing in input array!
+    temp.formSymmetricDifference(Set(array))
+    return Array(temp)
+}
+
+var testArray = Array(1...100)
+testArray.remove(at: 6)
+challenge40(array: testArray)
+
+
+/*:
+ [Previous](@previous)
+
+ # Challenge 41: Find the median
+ 
+ #### Difficulty: Easy
+
+ Write an extension to collections that accepts an array of Int and returns the median average,
+ or nil if there are no values.
+ Tip: The mean average is the sum of some numbers divided by how many there are. The median average is the middle of a sorted list. If there is no single middle value – e.g. if there are eight numbers - then the median is the mean of the two middle values.
+
+ 
+ #### Sample input and output
+ If your test array were created like this:
+
+ • The code [1, 2, 3].challenge41() should return 2.
+ 
+ • The code [1, 2, 9].challenge41() should return 2.
+ 
+ • The code [1, 3, 5, 7, 9].challenge41() should return 5.
+ 
+ • The code [1, 2, 3, 4].challenge41() should return 2.5.
+ 
+ • The code [Int]().challenge41() should return nil.
+ 
+ Then your method should return  [7, 21, 26], because those are the numbers missing from the array.
+
+ #### Hints
+ 
+ Hint #1: You’ll need to extend Collection with a specific constraint.
+ 
+ Hint #2: The method should return Double? because it might be a whole number, it might be a mean average of two numbers, or it might be nil if the collection is empty.
+ 
+ Hint #3: If you divide an odd integer by two, Swift will round down.
+ 
+ Hint #4: If you divide an even-numbered collection’s count by two, you’ll get the highest of the two values you need for your mean.
+ 
+ Hint #5: Make life easy for yourself: sort the collection first.
+
+ [Next](@next)
+*/
+
+
+
